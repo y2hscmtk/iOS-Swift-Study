7a03cb5ec8f71c91c1ddbf8184a98f6b47020418
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-10-02 19:05:33 +0900

    Section5-19 : Protocal & Delegate Practice

---
+++ UMC-Udemy-Study-Section5/UMC-Udemy-Study-Section5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC-Udemy-Study-Section5
+//
+//  Created by Choi76 on 2023/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UMC-Udemy-Study-Section5/UMC-Udemy-Study-Section5UITests/UMC_Udemy_Study_Section5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UMC_Udemy_Study_Section5UITestsLaunchTests.swift
+//  UMC-Udemy-Study-Section5UITests
+//
+//  Created by Choi76 on 2023/10/02.
+//
+
+import XCTest
+
+final class UMC_Udemy_Study_Section5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
