af7732dd33dcbfb2c9df76987567d3a8ac7a11c8
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-10-22 18:48:34 +0900

    Section 7-26 : 마이페이지 - 프로필 화면 구성

--- UMC-Udemy-Study/UMC-Udemy-Study/Cell/ProfileCollectionViewCell.swift
+++ UMC-Udemy-Study/UMC-Udemy-Study/Cell/ProfileCollectionViewCell.swift
@@ -10,10 +10,43 @@ import UIKit
 class ProfileCollectionViewCell: UICollectionViewCell {
     static let identifier = "ProfileCollectionViewCell"
 
+    //MARK: poperties
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var plusImageVIew: UIImageView!
+    
+    @IBOutlet weak var editButton: UIButton!
+    
+    @IBOutlet weak var addFriendButton: UIButton!
+    
+    @IBOutlet weak var followingCountLabel: UILabel!
+    @IBOutlet weak var postingCountLabel: UILabel!
+    @IBOutlet weak var followerCountLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setupAttributes()
     }
 
+    
+    //이미지뷰 둥글게 변경
+    private func setupAttributes(){
+        profileImageView.layer.cornerRadius = 88/2 //녹색 이미지뷰의 크기가 88이므로 2를 나눠주면 원이됨
+        
+        
+        //프로필 편집 버튼의 디자인 적용
+        editButton.layer.cornerRadius = 5 //버튼의 모양은 둥글게
+        editButton.layer.borderColor = UIColor.lightGray.cgColor //cgColor로 변환(버튼의 색은 라이트그레이로)
+        editButton.layer.borderWidth = 1 //테두리 두께는 1로 지정
+        
+        //친구 추가 버튼 디자인 적용
+        addFriendButton.layer.cornerRadius = 3 //버튼의 모양은 둥글게
+        addFriendButton.layer.borderColor = UIColor.lightGray.cgColor //cgColor로 변환(버튼의 색은 라이트그레이로)
+        addFriendButton.layer.borderWidth = 1 //테두리 두께는 1로 지정
+        
+        [postingCountLabel, followerCountLabel, followingCountLabel]
+            .forEach{ $0?.text = "\(Int.random(in: 0...10))"}
+    }
     
 }
--- UMC-Udemy-Study/UMC-Udemy-Study/ProfileViewController.swift
+++ UMC-Udemy-Study/UMC-Udemy-Study/ProfileViewController.swift
@@ -39,7 +39,7 @@ class ProfileViewController: UIViewController {
 
 extension ProfileViewController: UICollectionViewDelegate,UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        5
+        1 //셀 하나만 사용할 것임
     }
     
     
@@ -58,9 +58,16 @@ extension ProfileViewController: UICollectionViewDelegate,UICollectionViewDataSo
 }
 
 //컬렉션 뷰의 셀 사이즈 조절 => 테이블 뷰와 다른 방식을 사용함
+//nib파일은 ui작업을 도와주는 용도일 뿐이라, nib에서 커스텀을 진행해도 반영되지 않음
+//크기에 대한 작업은 프로토콜로 해야함
 extension ProfileViewController: UICollectionViewDelegateFlowLayout{
     
-    override func size(forChildContentContainer container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
-        <#code#>
+ 
+    //CGSize를 리턴함
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(
+            width: collectionView.frame.width,
+            height: CGFloat(159)
+        )
     }
 }
