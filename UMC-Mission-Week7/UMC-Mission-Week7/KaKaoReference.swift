12465dd9b27b378d0607987517f05b7a7bb285d3
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-14 11:33:34 +0900

    계정정보 추출 함수 작성

--- UMC-Mission-Week7/UMC-Mission-Week7/KaKaoReference.swift
+++ UMC-Mission-Week7/UMC-Mission-Week7/KaKaoReference.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+// Kakao api pod file
+import KakaoSDKAuth
+import KakaoSDKUser
+import KakaoSDKCommon
+
 // 카카오 계정과 관련된 작업을 수행하기 위한 싱글톤 객체
 class KaKaoReference{
     
@@ -15,21 +20,23 @@ class KaKaoReference{
     
     private init() {} //외부 생성 금지
     
-    
-    // 카카오톡 설치 여부 확인 => 카카오톡 앱을 통해 로그인을 구현할 경우
-//        if (UserApi.isKakaoTalkLoginAvailable()) {
-//            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-//                if let error = error {
-//                    print(error)
-//                }
-//                else {
-//                    print("loginWithKakaoTalk() success.")
-//
-//                    //do something
-//                    _ = oauthToken
-//                }
-//            }
-//        }
+    //사용자 정보 추출
+    func extractUserInfo(_ userInfo: User?) -> [String: Any]? {
+        print("call extractUserInfo")
+        guard let userInfo = userInfo, let properties = userInfo.properties else {
+            print("extractUserInfo Data binding failed")
+            return nil
+        }
+
+        // 딕셔너리로 추출한 데이터 반환
+        var extractedInfo: [String: Any] = [:]
+        extractedInfo["nickname"] = properties["nickname"]
+        extractedInfo["profileImage"] = properties["profile_image"]
+        extractedInfo["thumbnailImage"] = properties["thumbnail_image"]
+        //닉네임, 프로필이미지, 썸네일이미지 반환
+        return extractedInfo
+    }
+
     
     
 }
--- UMC-Mission-Week7/UMC-Mission-Week7/ViewController.swift
+++ UMC-Mission-Week7/UMC-Mission-Week7/ViewController.swift
@@ -31,6 +31,21 @@ class ViewController: UIViewController {
     //카카오 계정으로 시작하기 버튼 클릭시
     @IBAction func UIButtonLogInDidClicked(_ sender: UIButton) {
         
+        // 카카오톡 설치 여부 확인 => 카카오톡 앱을 통해 로그인을 구현할 경우
+//        if (UserApi.isKakaoTalkLoginAvailable()) {
+//            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+//                if let error = error {
+//                    print(error)
+//                }
+//                else {
+//                    print("loginWithKakaoTalk() success.")
+//
+//                    //do something
+//                    _ = oauthToken
+//                }
+//            }
+//        }
+        
         // 웹사이트에서 카카오톡 로그인 화면을 띄울 경우
         UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
                 if let error = error {
@@ -38,6 +53,7 @@ class ViewController: UIViewController {
                 }
                 else {
                     print("loginWithKakaoAccount() success.")
+
                     //do something
                     _ = oauthToken
                     print("oauthToken : \(String(describing: oauthToken))")
@@ -47,26 +63,17 @@ class ViewController: UIViewController {
                     UserApi.shared.me() {(user, error) in
                         if let error = error {
                             print(error)
-                        }
-                        else {
-                            //do something
-                            let userInfo = user //Type : Optional
+                        } else if let userInfo = KaKaoReference.shared.extractUserInfo(user) {
+                            
                             /*
                              //print("user Information : \(String(describing: userInfo))")
                              user Information : Optional(KakaoSDKUser.User(id: Optional(3161942434),
                              properties: Optional(["thumbnail_image": "https://k.kakaocdn.net/dn/cwepDk/btrX3ZvZSwU/FiZQcmjfVaogHsB3k9SwDk/img_110x110.jpg", "profile_image": "https://k.kakaocdn.net/dn/cwepDk/btrX3ZvZSwU/FiZQcmjfVaogHsB3k9SwDk/img_640x640.jpg", "nickname": "최강"]), kakaoAccount: Optional(KakaoSDKUser.Account(profileNeedsAgreement: nil, profileNicknameNeedsAgreement: Optional(false), profileImageNeedsAgreement: Optional(false), profile: Optional(KakaoSDKUser.Profile(nickname: Optional("최강"), profileImageUrl: Optional(https://k.kakaocdn.net/dn/cwepDk/btrX3ZvZSwU/FiZQcmjfVaogHsB3k9SwDk/img_640x640.jpg), thumbnailImageUrl: Optional(https://k.kakaocdn.net/dn/cwepDk/btrX3ZvZSwU/FiZQcmjfVaogHsB3k9SwDk/img_110x110.jpg), isDefaultImage: Optional(false))), nameNeedsAgreement: nil, name: nil, emailNeedsAgreement: nil, isEmailValid: nil, isEmailVerified: nil, email: nil, ageRangeNeedsAgreement: nil, ageRange: nil, birthyearNeedsAgreement: nil, birthyear: nil, birthdayNeedsAgreement: nil, birthday: nil, birthdayType: nil, genderNeedsAgreement: nil, gender: nil, phoneNumberNeedsAgreement: nil, phoneNumber: nil, ciNeedsAgreement: nil, ci: nil, ciAuthenticatedAt: nil, legalNameNeedsAgreement: nil, legalName: nil, legalBirthDateNeedsAgreement: nil, legalBirthDate: nil, legalGenderNeedsAgreement: nil, legalGender: nil, isKoreanNeedsAgreement: nil, isKorean: nil)), groupUserToken: nil, connectedAt: Optional(2023-11-14 01:35:22 +0000), synchedAt: nil, hasSignedUp: nil))
                             */
-                            //optional binding
-                            if let userInfo = userInfo {
-                                if let properties = userInfo.properties{
-                                    print("### properties : \(properties) ")
-                                }
-                                
-                            }
                             
-                            
-                            
+                            print("### Extracted User Info: \(userInfo)")
                         }
+                        
                     }
                     
                     
