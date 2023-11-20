55f79e29f0991fdf7a382e6795cfe82364556e98
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-20 17:29:29 +0900

    project init

---
+++ UMC-Mission-Week8/UMC-Mission-Week8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC-Mission-Week8
+//
+//  Created by Choi76 on 2023/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
