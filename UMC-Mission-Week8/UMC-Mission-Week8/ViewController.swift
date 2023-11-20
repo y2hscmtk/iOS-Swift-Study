a20e9b2b3a7b39edacefd5c56d246a5fea8fd449
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-20 19:24:59 +0900

    회원가입 화면 이동

--- UMC-Mission-Week8/UMC-Mission-Week8/ViewController.swift
+++ UMC-Mission-Week8/UMC-Mission-Week8/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idLabel: UITextField! //사용자가 입력한 아이디
+    @IBOutlet weak var pwLabel: UITextField! //사용자가 입력한 비밀번호
+    
+    
+    
+    // 회원 가입 버튼 클릭됨
+    @IBAction func signUpBtnDidTapped(_ sender: Any) {
+        // 스토리보드 아이디 찾기
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // 이동할 화면 찾기
+        guard let signUpViewController = storyboard
+            .instantiateViewController(withIdentifier: "SignUpViewController") as? SignUpViewController else { return }
+        // 화면 이동
+        self.navigationController?.pushViewController(signUpViewController, animated: true)
+        
+    }
+    
+    
+    @IBAction func loginBtnDidTapped(_ sender: Any) {
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
 
 }
 
