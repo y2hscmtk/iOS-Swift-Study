e7500400dac9d7805c37fdcc15ed367e2153867b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-08 18:55:42 +0900

    ADD : DrugSearchVC, DrugInfoVC

---
+++ UMC-Mission-Week9-Drug-Info/UMC-Mission-Week9-Drug-Info/DrugInfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DrugInfoViewController.swift
+//  UMC-Mission-Week9-Drug-Info
+//
+//  Created by Choi76 on 2023/12/08.
+//
+
+import UIKit
+
+// API를 통해 얻어온 알약 정보를 화면에 보여준다.
+class DrugInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ UMC-Mission-Week9-Drug-Info/UMC-Mission-Week9-Drug-Info/DrugSearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DrugSearchViewController.swift
+//  UMC-Mission-Week9-Drug-Info
+//
+//  Created by Choi76 on 2023/12/08.
+//
+
+import UIKit
+
+
+// 알약을 검색하는 페이지
+// 검색 결과를 테이블 뷰를 사용하여 보여준다.
+class DrugSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
