fbd7df0f95fa64adb366dc26fcbc8241fd325ef2
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-08 18:37:13 +0900

    Project init

---
+++ UMC-Mission-Week9-Drug-Info/UMC-Mission-Week9-Drug-Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC-Mission-Week9-Drug-Info
+//
+//  Created by Choi76 on 2023/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
