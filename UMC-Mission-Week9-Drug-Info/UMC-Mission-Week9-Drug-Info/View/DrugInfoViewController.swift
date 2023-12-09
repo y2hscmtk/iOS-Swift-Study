f7c483e56463e8753c363bdebf52e0c7179cc4aa
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-09 20:51:11 +0900

    ADD : DrugDetailInfo Page - DrugInfoViewController

--- UMC-Mission-Week9-Drug-Info/UMC-Mission-Week9-Drug-Info/View/DrugInfoViewController.swift
+++ UMC-Mission-Week9-Drug-Info/UMC-Mission-Week9-Drug-Info/View/DrugInfoViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 // 화면에 보여줄 데이터
 class DrugInfoViewController: UIViewController {
 
+    var drug : Drug? // 전달 받은 알약 데이터
+    
     // 알약 이미지(없을수도 있음)
     @IBOutlet weak var drugImage: UIImageView!
     // 제조사
@@ -18,14 +20,19 @@ class DrugInfoViewController: UIViewController {
     // 알약 이름
     @IBOutlet weak var drugName: UILabel!
     // 복용 효능(요약)
+    @IBOutlet weak var efficient: UILabel!
     // 복용 방법
+    @IBOutlet weak var howToUse: UILabel!
     // 보관 방법
+    @IBOutlet weak var howToStore: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // 데이터 삽입
     }
 
 
