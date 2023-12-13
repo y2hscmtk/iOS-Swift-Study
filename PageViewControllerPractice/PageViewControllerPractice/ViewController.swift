d281ceb1b48ac4dc3c0dba82fb17917821f3407b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-14 00:45:06 +0900

    ADD : NavigationView to ViewController

--- PageViewControllerPractice/PageViewControllerPractice/ViewController.swift
+++ PageViewControllerPractice/PageViewControllerPractice/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    // pageviewController화면으로 이동
+    @IBAction func showPageView(_ sender: Any) {
+        let stoaryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        // 이동할 화면 찾기
+        let pageVC = stoaryboard.instantiateViewController(identifier: "MyPageViewController")
+        
+        // 화면 이동
+        self.navigationController?.pushViewController(pageVC, animated: true)
+    }
+    
 }
 
