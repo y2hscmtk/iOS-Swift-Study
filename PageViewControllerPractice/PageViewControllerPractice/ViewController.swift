99fdfab8a04b11223104a90f5a5f2dfe2fe2e15f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-13 23:53:06 +0900

    Project init - PageViewControllerPractice

---
+++ PageViewControllerPractice/PageViewControllerPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageViewControllerPractice
+//
+//  Created by Choi76 on 2023/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
