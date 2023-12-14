e1ad67fb1f2c73e58fa01184770d92ceae99cc56
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-15 00:22:24 +0900

    FEAT : 상단 부분 고정 => UIView를 ContainerView로 사용

--- PageViewControllerPractice/PageViewControllerPractice/ViewController.swift
+++ PageViewControllerPractice/PageViewControllerPractice/ViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // uiview를 MyPageViewController의 컨테이너로 사용
+    @IBOutlet weak var containerView: UIView!
+    
+    // containerView에 할당할 MyPageViewController
+    var pageViewController: UIPageViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 스토리보드에서 MyPageViewController 인스턴스화
+        if let pageVC = storyboard?.instantiateViewController(withIdentifier: "MyPageViewController") as? MyPageViewController {
+            self.pageViewController = pageVC
+            self.addChild(pageVC) // 자식 뷰 컨트롤러로 추가
+            self.containerView.addSubview(pageVC.view) // containerView에 뷰 추가
+
+            // MyPageViewController의 뷰 크기 및 위치 조정
+            pageVC.view.frame = containerView.bounds
+            pageVC.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            pageVC.didMove(toParent: self)
+        }
     }
 
-    // pageviewController화면으로 이동
-    @IBAction func showPageView(_ sender: Any) {
-        let stoaryboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        // 이동할 화면 찾기
-        let pageVC = stoaryboard.instantiateViewController(identifier: "MyPageViewController")
-        
-        // 화면 이동
-        self.navigationController?.pushViewController(pageVC, animated: true)
-    }
+    
     
 }
 
